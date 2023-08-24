8caa287c120f0c375b8812435f99e7230ef95664
Petar Vico <petarvico3@outlook.com>
2023-08-24 11:50:37 -0400

    remove quick info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,13 @@ import { AppComponent } from './app.component';
 import { ProfilePictureComponent } from './profile-picture/profile-picture.component';
 import { SocialLinksComponent } from './social-links/social-links.component';
 import { AboutMeComponent } from './about-me/about-me.component';
-import { QuickInfoComponent } from './quick-info/quick-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfilePictureComponent,
     SocialLinksComponent,
-    AboutMeComponent,
-    QuickInfoComponent
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
